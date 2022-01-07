2c7b184d2fd8417c8140c0f3f4f65f5779f63f82
Eren Şimşek <57567358+Aporlorxl23@users.noreply.github.com>
2022-01-07 21:47:22 +0300

    Create IDK.asm

---
+++ IDK.asm
@@ -0,0 +1,37 @@
+section .text
+    global _start
+
+_IDK:
+    push rbp
+    mov rbp, rsp
+    mov eax, [rsp+16]
+    mov ecx, 3
+    mul ecx
+    leave
+    ret
+
+_start:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 8
+
+    push 23
+    call _IDK
+    add rsp, 8
+
+    mov DWORD [num], eax
+    add DWORD [num+1], 0xA
+
+    mov eax, 1
+    mov edi, 1
+    mov esi, num
+    mov edx, 2
+    syscall
+
+    leave
+    mov eax, 60
+    xor edi, edi
+    syscall
+
+section .bss
+    num: resd 2
